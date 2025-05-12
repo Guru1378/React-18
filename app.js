ca32a7107dcc9c231681f0093d60ca9b8628ae21
GURU PRASAD NOOLIVEEDU <guruprasadndvsvce2021csm@gmail.com>
2025-05-12 15:05:08 +0530

    Conditional CSS

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";  // Correct import for React 18+
-import Increment from "./increment";
+import Derived_data from "./DerivedState";
 /*const header = (
     <h1>Hello Guru</h1>
 );*/
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Increment/>);
+root.render(<Derived_data/>);
